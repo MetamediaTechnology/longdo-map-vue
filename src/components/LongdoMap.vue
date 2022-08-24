6f64f29831688986cd5c98879c897259c1efbba7
ravinwang <variin42@gmail.com>
2022-08-24 10:05:42 +0700

    Update LongdoMap.vue

--- src/components/LongdoMap.vue
+++ src/components/LongdoMap.vue
@@ -1,54 +1,53 @@
 <template>
-<div class="ldmap-vue-container" >
-  <div class="ldmap-vue-placeholder" ref="map" />
-  <slot />
-</div>
+  <div class="ldmap-vue-container">
+    <div class="ldmap-vue-placeholder" ref="map" />
+    <slot />
+  </div>
 </template>
 
 <script>
-import mapOptions from '@/mixins/mapOptions'
-import manager from '@/manager'
+import mapOptions from "@/mixins/mapOptions";
+import manager from "@/manager";
 
 export default {
-  name: 'LongdoMap',
+  name: "LongdoMap",
   mixins: [mapOptions],
-  data () {
+  data() {
     return {
       map: null,
-      mapPromise: null
-    }
+      mapPromise: null,
+    };
   },
-  created () {
-    let self = this
-    this.mapPromise = manager.promise
+  created() {
+    let self = this;
+    this.mapPromise = manager.promise;
     this.mapPromise.then(() => {
-      manager.initLongdoMap(self.initLongdoMap)
-    })
+      manager.initLongdoMap(self.initLongdoMap);
+    });
   },
   methods: {
-    initLongdoMap () {
-      let options = this.getMapOptions()
-      options.placeholder = this.$refs.map
-      this.map = new window.longdo.Map(options)
-     
-      this.$emit('load', this.map)
-      
+    initLongdoMap() {
+      let options = this.getMapOptions();
+      options.placeholder = this.$refs.map;
+      this.map = new window.longdo.Map(options);
+      if (options.zoomRange) {
+        this.map.zoomRange(options.zoomRange);
+      }
+      this.$emit("load", this.map);
+
       this.map.Event.bind("ready", () => {
         this.$emit("mapReady");
-        if (options.zoomRange) {
-          this.map.zoomRange(options.zoomRange);
-        } 
       });
-    }
+    },
   },
-  init ({ apiKey, src = undefined, checkApiKey = true }) {
+  init({ apiKey, src = undefined, checkApiKey = true }) {
     manager.load({
       apiKey: apiKey,
       src: src,
-      checkApiKey: checkApiKey
-    })
-  }
-}
+      checkApiKey: checkApiKey,
+    });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -57,7 +56,7 @@ export default {
   width: 100%;
   height: 100%;
   position: relative;
-  font-family: Tahoma,sans-serif;
+  font-family: Tahoma, sans-serif;
 }
 .ldmap-vue-placeholder {
   width: 100%;
